45d704bd6f6832d1de1cc6e518d4ff4195dc83d5
KYHSGeekCode <jourhyang123@gmail.com>
2022-01-02 19:06:59 +0900

    :tada: Begin project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Hello from "./Hello.tsx";
+import Wrapper from "./Wrapper.tsx";
+import Counter from "./Counter.tsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = '응애';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24, // 기본 단위 px
+        padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+    }
+
+
+    return (
+        <>
+            <Hello name={"응애"} color={"red"}/>
+            <Hello name={"응애 비구조화 할당"} color={"blue"}/>
+            <Hello color={"green"}/>
+            <Wrapper>
+                <Hello color={"green"}/>
+                <Counter/>
+            </Wrapper>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Hello.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hello({color, name}) {
+    return <div style={{color}}>{name}</div>
+}
+
+Hello.defaultProps = {
+    name: '이름없음'
+}
+
+
+export default Hello;
